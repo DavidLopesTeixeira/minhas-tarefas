d4386a621d80641e406b4849555baca26696ca36
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2024-06-04 12:38:51 -0300

    Configuração do Redux
    Aula 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+import { Provider } from 'react-redux'
 import EstiloGlobal, { Container } from './Styles'
 import BarraLateral from './containers/BarraLateral'
 import ListaDeTarefas from './containers/ListaDeTarefa'
+import store from './store'
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <EstiloGlobal />
       <Container>
         <BarraLateral />
         <ListaDeTarefas />
       </Container>
-    </>
+    </Provider>
   )
 }
 
--- src/containers/ListaDeTarefa/index.tsx
+++ src/containers/ListaDeTarefa/index.tsx
@@ -1,43 +1,27 @@
+import { useSelector } from 'react-redux'
+
 import Tarefa from '../../Components/Tarefa'
 import { Container } from './styles'
-import * as enums from '../../Utils/enums/Tarefa'
+import { RootReducer } from '../../store'
 
-const tarefas = [
-  {
-    titulo: 'Estudar TypeScript',
-    descricao: 'Ver a aula 3 da EBAC',
-    prioridade: enums.Prioridade.IMPORTANTE,
-    status: enums.Status.PENDENTE
-  },
-  {
-    titulo: 'Pagar a conta de internet',
-    descricao: 'Baixar fatura no Gmail',
-    prioridade: enums.Prioridade.URGENTE,
-    status: enums.Status.CONCLUIDA
-  },
-  {
-    titulo: 'Ir para a academia',
-    descricao: 'Fazer treino A',
-    prioridade: enums.Prioridade.IMPORTANTE,
-    status: enums.Status.PENDENTE
-  }
-]
-
-const ListaDeTarefas = () => (
-  <Container>
-    <ul>
-      {tarefas.map((trf) => (
-        <li key={trf.titulo}>
-          <Tarefa
-            descricao={trf.descricao}
-            titulo={trf.titulo}
-            prioridade={trf.prioridade}
-            status={trf.status}
-          />
-        </li>
-      ))}
-    </ul>
-  </Container>
-)
+const ListaDeTarefas = () => {
+  const { tarefas } = useSelector((state: RootReducer) => state)
+  return (
+    <Container>
+      <ul>
+        {tarefas.map((trf) => (
+          <li key={trf.titulo}>
+            <Tarefa
+              descricao={trf.descricao}
+              titulo={trf.titulo}
+              prioridade={trf.prioridade}
+              status={trf.status}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
 
 export default ListaDeTarefas
