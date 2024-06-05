6c8ac1ee7a3cd85d235e0c28d59fe53827a198ca
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2024-06-04 21:16:57 -0300

    desenvolvimento da função que permite buscar por tarefas
    Aula 11

--- src/containers/ListaDeTarefa/index.tsx
+++ src/containers/ListaDeTarefa/index.tsx
@@ -6,10 +6,20 @@ import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
   const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  const filtraTarefas = () => {
+    return itens.filter(
+      (item) => item.titulo.toLowerCase().search(termo.toLowerCase()) >= 0
+    )
+  }
+
   return (
     <Container>
+      <p> 2 tarefas marcadas como: {termo}</p>
       <ul>
-        {itens.map((trf) => (
+        {/* {itens.map((trf) => ( */}
+        {filtraTarefas().map((trf) => (
           <li key={trf.titulo}>
             <Tarefa
               id={trf.id}
